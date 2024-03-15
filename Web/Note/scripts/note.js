68c2b8e920735e990c06a7d210f4010f08fd3b7f
YuanXiQWQ <xjr815226031@outlook.com>
2024-03-15 08:04:42 -0700

    JS119

--- Web/Note/scripts/note.js
+++ Web/Note/scripts/note.js
@@ -215,7 +215,10 @@
     * this会指向一个对象
     *    1. 作为函数调用时，指向的是window
     *    2. 作为方法调用时，指向的是调用方法的对象
-    * 箭头函数没有自己的this,它的this由外层作用域决定
+    *    3. 作为构造函数调用时，指向的是实例对象
+    *    4. 箭头函数没有自己的this,由外层作用域决定
+    *    5. 通过call()和apply()方法调用的函数,它们的第一个参数就是函数的this
+    *    6. 通过bind()返回的函数,this由bind()第一个参数决定,且无法修改
     * */
 }
 {
@@ -293,6 +296,20 @@
     *       (起始位置(包含),结束位置(不包含))    从起始到结束
     *       (起始位置(包含))                  从起始到数组末位
     *       ()                              浅拷贝数组
+    *   sort():指定数组排序规则并排序数组,传入回调函数作为参数
+    *   forEach():遍历数组
+    *       需要一个回调函数作为参数,这个回调函数会被调用数组元素个数次,每次调用都将数组中的数据作为参数传递
+    *       回调函数有三个参数
+    *            1. 当前遍历的元素(element)
+    *            2. 当前遍历的索引(index)
+    *            3. 被遍历的数组(array)
+    *   filter():将数组中符合条件的函数保存到一个新数组中.传入回调函数作为条件参数,当函数返回值为true时保留元素,打到过滤的效果
+    *   map():根据当前数组生成一个新数组.
+    *        需要一个回调函数作为参数,回调函数的返回值会成为新数组中的元素.
+    *   reduce():可以用来将一个数组中的所有元素整合为一个值
+    *        参数:
+    *           1. 回调函数,通过回调函数来制定合并的规则
+    *           2. (可选)初始值
     *
     * 破坏性方法
     *   push():向数组末位添加元素,并返回新数组的长度
@@ -311,8 +328,48 @@
         arr.splice(2, 2, 5, 6);
         console.log(arr);
     }
-    //数组的去重:
     {
+        //sort()
+        /* sort()
+        *  用来对数组进行排序
+        *  会按照回调函数的返回值进行排序,但是默认是先转换为字符串,然后按照Unicode编码进行排序
+        *   参数:
+        *       可以船体一个回调函数作为参数,通过回调函数来制定排序规则
+        *           (a, b) => a - b //升序排列
+        *           (a, b) => b - a //降序排列
+        * */
+        arr1 = [6, 6, 2, 6, 0, 7, 0, 1, 0, -15];
+        arr1.sort((a, b) => {
+            return a - b;
+        });
+        console.log(arr1);
+    }
+    {
+        //filter
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = arr.filter((item) => {
+            return item > 5;
+        });
+        console.log(result);
+    }
+    {
+        //map
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = arr.map((item) => {
+            return item * 2;
+        });
+        console.log(result);
+    }
+    {
+        //reduce
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = arr.reduce((a, b) => {
+            return a + b;
+        });
+        console.log(result);
+    }
+    {
+        //数组的去重:
         arr = [1, 1, 4, 5, 1, 4, 1, 9, 1, 9, 8, 1, 6];
         for (let i = 0; i < arr.length; i++) {
             for (let j = i + j; j < arr.length; j++) {
@@ -324,4 +381,186 @@
         }
         console.log(arr);
     }
+}
+{
+    //闭包
+    /*使用时机:
+        需要将访问内容隐藏,防止被以外更改
+    * 构成闭包的条件
+    *   1. 函数的嵌套
+    *   2. 内部函数要引用外部函数中的变量
+    *   3. 内部函数要作为外部返回值返回
+    * */
+
+    //闭包函数
+    function outer() {
+        let num = 0;
+
+        //1.嵌套;3.返回一个匿名函数
+        return () => {
+            //2.访问外部函数的变量
+            num++;
+            console.log(num);
+        }
+    }
+
+    //调用
+    const newFunction = outer();
+
+    //执行十次
+    for (let i = 0; i < 10; i++) {
+        newFunction();
+    }
+}
+{
+    //可变参数
+    /* 除了this以外,function中还有一个隐含参数,即arguments
+    *  arguments是一个类数组对象(伪数组),用来存储函数的实参.
+    *   `   和数组类似,可以通过索引来读取元素,也可以通过for循环变量,但是它不一定是一个数组对象,不能调用数组方法
+    *   无论用户是否定义形参,实参都会存储到arguments对象中.
+    *   可以通过该对象直接访问实参
+    *   无论是形参还是实参都可以传递
+    * 不足:
+    *   1. 无法传递对象
+    *   2. 无法传递函数
+    *   3. 无法传递类型不同的实参
+    * */
+
+
+    //定义一个函数,可以求任意个数值的和
+    function sum1() {
+        let result = 0;
+        for (let num of arguments) {
+            result += num;
+        }
+        return result;
+    }
+
+    console.log(sum1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+    /* 可变参数:在函数中可以传入任意个数的实参
+    *       可变参数可以接受任意数量的实参,并将他们统一存储到一个数组中返回
+    *       可变参数的作用域和arguments基本一致,但也有一些不同
+    *           1. 可变参数的名字可以自己指定
+    *           2. 可变参数就是一个数组,可以直接使用数组方法
+    * */
+    function testArgs(...args) {
+        console.log(args);
+    }
+
+    function sum2(...nums) {
+        return nums.reduce((a, b) => {
+            return a + b;
+        });
+    }
+
+    console.log(sum2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+}
+{
+    // call和apply
+    /*  函数名.call()和函数名.apply()方法和 函数名() 一样,都可以调用对应函数,不同的是
+    *   call()和apply()方法还可以用来指定函数中的this
+    *   call()和apply()的第一个参数将会成为函数的this
+    *   call()方法调用函数,函数的实参直接在第一个参后一个一个地列出来
+    *   通过apply()方法调用函数,函数的实参需要通过一个数组传递
+    * */
+
+    function fn(a, b) {
+        console.log("a = " + a, "b = " + b);
+    }
+
+    fn.apply(obj, ["hello", "world"]);
+}
+{
+    //bind()
+    /*  bind()是函数的方法,可以用来创建一个新的函数
+            bind()可以为新函数绑定this
+            bind()可以为新函数绑定参数
+        箭头函数没有自身的this,它的this由外层作用域决定,所以无法通过call(),apply()和bind()方法绑定this
+        箭头函数也没有arguments
+    * */
+    function testBind(a, b, c, d) {
+        console.log(this);
+        console.log(a, b, c, d);
+    }
+
+    const obj = {name: "MUTT"};
+    const obj2 = {name: "TT"};
+
+    const newFunc = testBind.bind(obj, 1, 2, 3, 4);
+
+    newFunc.call(obj2, 5, 6, 7, 8);
+
+    const arrowFunc = (a, b, c, d) => {
+        console.log(this);
+        console.log(a, b, c, d);
+    }
+    const newArrowFunc = arrowFunc.bind(obj, 1, 2, 3, 4);
+    newArrowFunc.call(obj2, 5, 6, 7, 8);
+}
+{
+    //解构赋值
+    /*解构数组*/
+    const arr = [1, 2, 3, 4, 5];
+    const [a, b, ...rest] = arr;
+    console.log(a, b, rest);
+
+    /*解构对象*/
+    const obj = {a: 1, b: 2, c: 3};
+    const {a: aa, b: bb, c: cc} = obj;
+    console.log(aa, bb, cc);
+
+    /*交换位置*/
+    let a1 = 1;
+    let a2 = 2;
+
+    [a1, a2] = [a2, a1];
+    console.log(a1, a2);
+}
+{
+    //对象的序列化
+    /*将对象转换为字符串
+    *   不仅JS能看懂,人类也能,其它语言也能看懂,可以用作对象的传递
+    * 如何进行序列化:
+    *   JS的工具:JSON
+    *   使用JSON.stringify()方法把JS对象转换为字符串
+    *   使用JSON.parse()方法把字符串转换为JS对象
+    * */
+    const obj = {
+        name: "MUTT", age: 18
+    };
+    const str = JSON.stringify(obj);
+    console.log(str);
+
+    const obj2 = JSON.parse(str);
+    console.log(obj2);
+
+    console.log(obj === obj2);
+
+    /*JSON的编写
+    *   1.JSON字符串有两种类型:
+    *       JSON对象 {}
+    *       JSON数组 []
+    *   2.JSON的属性名必须用双引号括起来
+    *   3.JSON中可以使用的属性值(元素):
+    *       数字(Number)
+    *       字符串(String),必须使用双引号
+    *       布尔值(Boolean)
+    *       null
+    *       JSON对象(Object{})
+    *       JSON数组(Array[])
+    *   4.JSON的格式和JS的格式基本一致,但是JSON更严格
+    * */
+    const str1 = '{"name": "MUTT", "age": 18}';
+    const obj3 = JSON.parse(str1);
+    console.log(obj3);
+}
+{
+    //Map
+    /*Map用来存储键值对解构的数据(key-value)
+    Object中存储的数据就可以被认为是一种键值对解构
+    Map和Object的主要区别:
+        Object中的属性名只能说字符串或符号,如果传递了一个其他类型的属性名,JS解释器会自动将其转换为字符串
+        Map中任何类型的值都可以成为数据的key
+    * */
 }
